e6f3e3f7fabb4e9d24dd3d131bca768996d0d510
caseylee8008 <caseybraylinlee@gmail.com>
2019-08-20 14:15:07 +0000

    Copy over files from Monday

---
+++ HelloName.java
@@ -0,0 +1,8 @@
+public class HelloName
+{
+    public static void main(String[] args)
+    {
+        String name = "Casey Dad Lee";
+        System.out.println("Hello, " + name);
+    }
+}
---
+++ PrintSomething.java
@@ -0,0 +1,8 @@
+public class PrintSomething
+{
+    public static void main(String[] args)
+    {
+        String theTruth = "Vincent likes men";
+        System.out.println("The truth is, " + theTruth);
+    }
+}
