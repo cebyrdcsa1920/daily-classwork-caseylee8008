3adad4a3090192cc1df202c3fff826f119826e46
caseylee8008 <caseybraylinlee@gmail.com>
2019-08-27 14:10:07 +0000

    Add a main to student

--- week3/Student.java
+++ week3/Student.java
@@ -9,7 +9,13 @@ public class Student
     {
         this.gpa = 0.0;
         this.name = name;
-        this.studentID = studentID
+        this.studentID = studentID;
         this.age = age;
+    }
+
+    public static void main(String[] args)
+    {
+        Student casey = new Student("Casey","672875",17);
+        System.out.println(casey);
     }
 }
