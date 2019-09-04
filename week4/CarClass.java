3125f545cd9103c72630ce24f3dd3b5c7047ba64
caseylee8008 <caseybraylinlee@gmail.com>
2019-09-04 13:46:30 +0000

    Make a car constructor

--- week4/CarClass.java
+++ week4/CarClass.java
@@ -1,4 +1,20 @@
+import java.awt.Color;
+
 public class CarClass
 {
-    public class void main(String[] args)
+    private String model;
+    private Color color;
+    private int numPassengers;
+    private double amountOfGas;
+
+    public CarClass()
+    {
+        model = "Mustang";
+        color = Color.YELLOW;
+        numPassengers = 2;
+        amountOfGas = 12.4;
+
+    }
+
+
 }
